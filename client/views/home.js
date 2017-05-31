fdf62dbb846513ca68d73ba8d2b86d8590e1e112
trof <reatrof@gmail.com>
2017-05-31 13:55:46 +0300

    router.js content

--- client/views/home.js
+++ client/views/home.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export default class Home extends React.Component {
-    render() {
-        return (
-            <h1> Hello World </h1>
-        )
-    }
-}
+const Home = () => (
+    <h1>Hello World</h1>
+)
+
+export default Home;
