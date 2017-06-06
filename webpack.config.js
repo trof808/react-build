ecbef43782073f07ec6fd25cfe513baea544f350
trof <reatrof@gmail.com>
2017-06-06 14:47:39 +0300

    webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './client/index.js',
@@ -14,5 +15,21 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            inject: 'body'
+        })
+    ]
 };
